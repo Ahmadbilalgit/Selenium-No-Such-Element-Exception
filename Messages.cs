e36eb7699a3d1cba2245822a34e659bf397b0d19
Ahmed Bilal <ahmed.bilal@netrevelation.com>
2020-01-08 12:33:07 +0500

    First commit

---
+++ Messages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace No_Such_Element_Exception_Handle
+{
+    public static class Messages //Static class can only have static methods.
+
+    {
+        public static void Greenmsg(String green)
+
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            System.Console.WriteLine(green);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void Redmsg(String red)
+
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.WriteLine(red);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace No_Such_Element_Exception_Handle
+{
+    class Entrypoint
+    {
+        static void Main()
+        {
+
+
+            IWebDriver driver = new ChromeDriver("./");
+            //NuGet package which will copy the driver to the bin folder.
+            //Then you can provide path to the current folder to driver ctor to run your tests:
+            driver.Navigate().GoToUrl("http://116.203.214.214/");
+
+
+            IWebElement Userid = driver.FindElement(By.Name("email"));
+            Userid.Click();
+            Userid.SendKeys("test@abc.com");
+            // Thread.Sleep(3000);
+
+            IWebElement password = driver.FindElement(By.Name("password"));
+            password.Click();
+            password.SendKeys("abc");
+            //Thread.Sleep(3000);
+
+
+
+            IWebElement Button;
+            try
+            {
+                Button = driver.FindElement(By.XPath("/html/body/div/div/div/div/div/div/form/div[5]/button"));
+                // Change the above element address path , then you ll be able to witness the result. Test 
+
+                if (Button.Displayed)
+
+                {
+
+                    Messages.Greenmsg("The element found");
+
+                }
+
+                Button.Click();
+            }
+
+            catch (NoSuchElementException)
+            {
+
+                Messages.Redmsg("The element not found");
+
+            }
+            // Thread.Sleep(3000);
+            driver.Close();
+            driver.Quit();
+        }
+
+
+    }
+}
